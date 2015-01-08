63d0ad5f9a4e213f9b16de88da9d9b0c7a8f4638
Tobias Schultka <tobias.schultka@lovoo.com>
2015-01-08 02:04:10 +0100

    replaced tabs with spaces

--- pxlme.js
+++ pxlme.js
@@ -282,16 +282,16 @@ PXLME.Cursor = function ( radius ) {
 // provides request animation frame in a cross browser way
 // author: Paul Irish - http://paulirish.com
 if ( !window.requestAnimationFrame ) {
-	window.requestAnimationFrame = ( function () {
-		return window.requestAnimationFrame ||
+    window.requestAnimationFrame = ( function () {
+        return window.requestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame ||
             window.oRequestAnimationFrame ||
             window.msRequestAnimationFrame ||
             function( callback, element ) {
-			    window.setTimeout( callback, 1000 / 60 );
-		    };
-	})();
+                window.setTimeout( callback, 1000 / 60 );
+            };
+    })();
 }
 
 })( window );
